14de7a3a3ef746a8e862fbbdb56328db752f5483
PhamNgoc <email@gmail.com>
2026-08-21 23:36:49 +0700

    change 1

--- config.php
+++ config.php
@@ -2,6 +2,5 @@
 if (!defined('BASE_URL')) {
     define('BASE_URL', 'http://localhost/ThanhAM/');
 }
-
 define('SITE_NAME', 'THANH ÂM - Hệ Thống AI Cho Người Yếu Thế');
 ?>
--- connect.php
+++ connect.php
@@ -12,6 +12,5 @@ if ($conn->connect_error) {
     die("Kết nối CSDL thất bại: " . $conn->connect_error);
 }
 
-
 $conn->set_charset("utf8mb4");
 ?>
