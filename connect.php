4f27198fecebb21064d5e3893a792edfe8710450
PhamNgoc <email@gmail.com>
2026-08-21 23:33:12 +0700

    ketnoi

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__ . '/config.php';
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "thanham_db";
+
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+if ($conn->connect_error) {
+    die("Kết nối CSDL thất bại: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+?>
